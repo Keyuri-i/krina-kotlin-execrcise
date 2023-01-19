3d7cb651c0f56144a2cda93992c90eeec01b6f1c
krina13 <krinavc@infusionanalysts.com>
2023-01-19 16:02:24 +0530

    Design

--- app/src/main/java/com/example/basicprogram/MainActivity.kt
+++ app/src/main/java/com/example/basicprogram/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.example.basicprogram
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
-import android.widget.Button
-import android.widget.DatePicker
-import android.widget.Toast
-import java.util.Calendar
+import android.text.Html
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val textView:TextView=findViewById(R.id.txtCondition)
+        val html = ("<font color=" + Color.GRAY.toString() + ">I agree with </font><font color=" + Color.BLUE.toString() + "> " +
+                "Terms </font><font color="+ Color.GRAY.toString() + ">and </font><font color=" + Color.BLUE.toString() + "> Privacy</font>")
+        textView.text = Html.fromHtml(html)
+
     }
 }
+
