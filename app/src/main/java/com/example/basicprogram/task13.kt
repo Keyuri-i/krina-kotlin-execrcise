a1831d519805bae28ebebff1e8a6258a4923603e
krina13 <krinavc@infusionanalysts.com>
2023-01-20 17:38:14 +0530

    Design

--- app/src/main/java/com/example/basicprogram/task13.kt
+++ app/src/main/java/com/example/basicprogram/task13.kt
@@ -2,7 +2,7 @@ package com.example.basicprogram
 
 //remove duplicate in string
 
-fun main(args: Array<String>) {
+fun main() {
     val str = "geeksforgeeks".toCharArray()
     val dupli=str.distinct()
     println(dupli.toString())
